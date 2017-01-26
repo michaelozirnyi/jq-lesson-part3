c6bb2a4ce562fa4166f22383f8e6f9017a632bdc
Mykhailo <mykhailoozirnyi@gmail.com>
2017-01-26 15:53:03 +0200

    made autocomplete

--- js/script.js
+++ js/script.js
@@ -1,5 +1,31 @@
-jQuery(function () {    /*jQuery(function ($) {    */
+jQuery(function () {                     /*jQuery(function ($) {    */
 
+
+    jQuery('#find').autocomplete({
+
+        minChars: 1,
+        delimiter: /(,|;)\s*/,
+        maxHeight: 300,
+        width: 300,
+        zIndex: 9999,
+        deferRequestBy: 300,
+        lookup: [
+            "Андрей",
+            "Алексей",
+            "Александр",
+            "Арсений",
+            "Арсений",
+            "C",
+            "C++",
+            "Clojure",
+            "ColdFusion",
+            "Java",
+            "Java-a",
+            "Java-as",
+            "Java-asd",
+            "JavaScript"
+        ]
+    });
 
 
 
